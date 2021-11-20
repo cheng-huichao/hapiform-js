55a0a47edab7a8f45cb5260f54b068bf2e2f9d2c
cheng-huichao <cheng.huichao@yahoo.com>
2021-11-20 09:25:26 +0800

    remove some files

--- hapi-form/interface.js
+++
@@ -1,9 +0,0 @@
-System.register("hapi-interface", [], function (exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [],
-        execute: function () {
-        }
-    };
-});
