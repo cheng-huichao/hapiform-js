392a600e1dda9cbfa61e7e8b76f8a1b38c635a8e
cheng-huichao <cheng.huichao@yahoo.com>
2021-12-06 19:45:43 +0800

    update readme

---
+++ examples/site-contact-form.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <form id="hapi-form" class="contact-form">
+      <div class="">
+        <div class="contact-form-group">
+          <input id='name' name="name" type="text" class="contact-form-input" placeholder=" ">
+          <label for="name" class="contact-form-label">Name</label>
+        </div>
+      </div>
+
+      <div class="">
+        <div class="contact-form-group">
+          <input id='contact_no' name="contact" type="tel" class="contact-form-input" placeholder=" ">
+          <label for="contact_no" class="contact-form-label">Contact No.</label>
+        </div>
+      </div>
+
+      <div class="">
+        <div class="contact-form-group">
+          <input id='email' name="email" type="email" class="contact-form-input" placeholder=" ">
+          <label for="email" class="contact-form-label">Email</label>
+        </div>
+      </div>
+
+      <div class="">
+        <div class="contact-form-group">
+          <textarea id='message' name="message" class="contact-form-input" placeholder=" "></textarea>
+          <label for="message" class="contact-form-label">Message</label>
+        </div>
+      </div>
+      <div class="">
+        <button type="submit" class="relative mt-5 uppercase btn">
+          <span class="lg:text-lg">Submit</span>
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+// install hapiform.js first
+// yarn add hapiform.js
+
+import {hapiForm} from 'hapiform.js';
+
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+
+    hapiForm.setup([
+      {
+        formSelector: '#hapi-form',
+        hapiFormID: 'adaf0cf3-aaaa-aaaa-aaaa-e180e734f588',
+        redirectUrl: '/thank-you/',
+      }],
+    );
+
+  },
+};
+</script>
+
+<style scoped>
+
+.contact-form {
+  @apply relative space-y-3 lg:space-y-5;
+}
+
+.contact-form-group {
+  @apply relative rounded;
+}
+
+.contact-form-input {
+  @apply block px-3 py-2 w-full appearance-none focus:outline-none text-[#4b599e];
+  appearance: none;
+  z-index: 1;
+}
+
+.contact-form-label {
+  z-index: 0;
+  transform-origin: 0%;
+  @apply absolute top-0 bg-white rounded px-3 py-2 duration-300 text-[#4b599e];
+
+}
+
+
+.contact-form-input:focus-within ~ label,
+.contact-form-input:not(:placeholder-shown) ~ label {
+  @apply z-0 px-1 py-0 ml-3 transform scale-75 -translate-y-4;
+  transform-origin: 0%;
+}
+
+
+.hapi-form-input-error{
+
+}
+
+</style>
--- readme.md
+++ readme.md
@@ -1,14 +1,28 @@
-## Vanilla javascript library of Hapi Form
+## javascript library of Hapi Form
 
-```npm
+```bash
 npm i hapiform.js or yarn add hapiform.js
-``` 
-
-> Create hapi form from [here](https://hapiform.sg)
+```
 
 
-> Add reference of [hapiform.css](https://unpkg.com/hapiform.js@latest/hapiform.css) & [hapiform.js](https://unpkg.com/hapiform.js@latest/hapiform.js) unpkg cdn.
+### For ESM usage
 
+```javascript
+
+import {hapiForm} from 'hapiform.js';
+
+hapiForm.setup([
+  {
+    formSelector: '#hapi-form',
+    hapiFormID: 'adaf0cf3-aaaa-aaaa-aaaa-e180e734f588',
+    redirectUrl: '/thank-you/',
+  }]
+);
+
+
+```
+
+### For HTML project 
 ```html
 <!--unpkg cdn-->
 
@@ -17,18 +31,9 @@ npm i hapiform.js or yarn add hapiform.js
 <script src="https://unpkg.com/hapiform.js@latest/hapiform.js"></script>
 ```
 
-> Parameters of HapiForm - Array list. 
- 
-  * hapiFormID - required - form id, like: '40552337-abcd-abcd-abcd-e4bc28e9c874'
-  * formSelector? - optional - selector of Form Element. default: '#hapi-form'
-  * redirectUrl? - optional - redirect to page, default: '/thank-you.html'
-  * i18nLocale? - optional - i18n locale code
-   
-_parameters with `?` means it is optional parameter_
-
 ```javascript
 
-// set up one ot more forms
+// set up one or more forms
 let formConfig= [  
   {
     hapiFormID: '40552337-abcd-abcd-abcd-e4bc28e9c874', // todo: replace your real form Id
@@ -40,23 +45,19 @@ let formConfig= [
     redirectUrl: '/thank-you.html',
   },
 ]
-
-// use class to initial hapiform
-new HapiForm(formConfig)
-
-/* or */
-
 // use function to initial hapiform
 initHapiForm(formConfig);
 
-
-
 ```
- 
- 
 
-> Demo - Download the source code and open `index.html` you will see 2 forms as bellow:
+### Parameters
 
-![Multiple Enquiry Form Demo](demo.png)
+* hapiFormID - required - form id, like: '40552337-abcd-abcd-abcd-e4bc28e9c874'
+* formSelector? - optional - selector of Form Element. default: '#hapi-form'
+* redirectUrl? - optional - redirect to page, default: '/thank-you/'
+* i18nLocale? - optional - i18n locale code
+
+_parameters with `?` means it is optional parameter_
+
 
 
